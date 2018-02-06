0e112f1717c9e733c6361edc4045697d8759aa80
Ahmad <ataufiq665@gmail.com>
2018-02-06 14:39:38 +0700

    Final Making an App Interactive Part 1

--- Udacity Course/Basics User Interfaces/OrderCoffe/app/src/main/java/ataufiq/com/ordercoffe/MainActivity.java
+++ Udacity Course/Basics User Interfaces/OrderCoffe/app/src/main/java/ataufiq/com/ordercoffe/MainActivity.java
@@ -2,12 +2,53 @@ package ataufiq.com.ordercoffe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView quantityTxt,priceTxt;
+    Button orderBtn;
+    int quantity=1,price=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quantityTxt = findViewById(R.id.quantity_txt);
+        priceTxt    = findViewById(R.id.price_txt);
+        orderBtn    = findViewById(R.id.order_btn);
+
+
+
+    }
+
+    public void display(int number){
+        //Set text only get string so we must change int to string
+        quantityTxt.setText(String.valueOf(number));
+    }
+
+    public void displayPrice(int number){
+        priceTxt.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+    public void increment(View v){
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View v){
+        quantity--;
+        display(quantity);
+    }
+
+    public void orderButton(View v){
+        price=quantity*5;
+        displayPrice(price);
     }
 }
