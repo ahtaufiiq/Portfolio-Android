3d84429e647ea11c1623628dbc23eb4d27a19fa8
Ahmad <ataufiq665@gmail.com>
2018-02-07 13:50:38 +0700

    Lesson 6

--- Udacity Course/Basics User Interfaces/CourtCounter/app/src/main/java/ataufiq/com/courtcounter/MainActivity.java
+++ Udacity Course/Basics User Interfaces/CourtCounter/app/src/main/java/ataufiq/com/courtcounter/MainActivity.java
@@ -7,62 +7,62 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView scoreATxt,scoreBTxt;
-    int scoreTeamA,scoreTeamB;
+    TextView scoreATxt, scoreBTxt;
+    int scoreTeamA, scoreTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scoreATxt=findViewById(R.id.score_a_txt);
-        scoreBTxt=findViewById(R.id.score_b_txt);
+        scoreATxt = findViewById(R.id.score_a_txt);
+        scoreBTxt = findViewById(R.id.score_b_txt);
     }
 
-    public void displayScoreA(int score){
-     scoreATxt.setText(""+score);
+    public void displayScoreA(int score) {
+        scoreATxt.setText("" + score);
     }
 
-    public void plus3TeamA(View v){
-        scoreTeamA+=3;
+    public void plus3TeamA(View v) {
+        scoreTeamA += 3;
         displayScoreA(scoreTeamA);
     }
 
-    public void plus2TeamA(View v){
-        scoreTeamA+=2;
+    public void plus2TeamA(View v) {
+        scoreTeamA += 2;
         displayScoreA(scoreTeamA);
     }
 
-    public void plus1TeamA(View v){
+    public void plus1TeamA(View v) {
         scoreTeamA++;
         displayScoreA(scoreTeamA);
     }
 
     //------------------------------------------
 
-    public void displayScoreB(int score){
-        scoreBTxt.setText(""+score);
+    public void displayScoreB(int score) {
+        scoreBTxt.setText("" + score);
     }
 
-    public void plus3TeamB(View v){
-        scoreTeamB+=3;
+    public void plus3TeamB(View v) {
+        scoreTeamB += 3;
         displayScoreB(scoreTeamB);
     }
 
-    public void plus2TeamB(View v){
-        scoreTeamB+=2;
+    public void plus2TeamB(View v) {
+        scoreTeamB += 2;
         displayScoreB(scoreTeamB);
     }
 
-    public void plus1TeamB(View v){
+    public void plus1TeamB(View v) {
         scoreTeamB++;
         displayScoreB(scoreTeamB);
     }
 
     //----------------------------
-    public void reset(View v){
-        scoreTeamB=0;
-        scoreTeamA=0;
+    public void reset(View v) {
+        scoreTeamB = 0;
+        scoreTeamA = 0;
         displayScoreB(scoreTeamB);
         displayScoreA(scoreTeamA);
     }
--- Udacity Course/Basics User Interfaces/OrderCoffe/app/src/main/java/ataufiq/com/ordercoffe/MainActivity.java
+++ Udacity Course/Basics User Interfaces/OrderCoffe/app/src/main/java/ataufiq/com/ordercoffe/MainActivity.java
@@ -1,17 +1,23 @@
 package ataufiq.com.ordercoffe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView quantityTxt,priceTxt;
+    TextView quantityTxt;
     Button orderBtn;
+    CheckBox wippedCream,chocolate;
+    EditText name;
     int quantity=1;
 
     @Override
@@ -20,24 +26,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quantityTxt = findViewById(R.id.quantity_txt);
-        priceTxt    = findViewById(R.id.price_txt);
+
         orderBtn    = findViewById(R.id.order_btn);
 
+        wippedCream =findViewById(R.id.wipped_checkbox);
+        chocolate   = findViewById(R.id.chocolate_checkbox);
 
+        name    =findViewById(R.id.name_edit_text);
 
     }
 
     public void display(int number){
         //Set text only get string so we must change int to string
         quantityTxt.setText(String.valueOf(number));
-    }
-
-    public void displayPrice(int number){
-        priceTxt.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    private void displatMessage(String message){
-        priceTxt.setText(message);
     }
 
     public void increment(View v){
@@ -51,7 +52,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void orderButton(View v){
-        String priceMessage="Total: $"+ quantity*5+"\nThank You";
-        displatMessage(priceMessage);
+        String nama =name.getText().toString();
+
+        boolean hasWippedCream  = wippedCream.isChecked();
+        boolean hasChocolate    = chocolate.isChecked();
+
+        int price= calculatePrice(hasWippedCream,hasChocolate);
+
+        String message=createOrderSummary(nama,price,hasWippedCream,hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Order Coffe");
+        intent.putExtra(intent.EXTRA_TEXT,message);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public String createOrderSummary(String nama,int price,boolean addWippedCream,boolean addChocolate){
+        String message="Name: "+nama+
+                        "\nQuantity: "+quantity+
+                        "\nWhipped Cream: "+ addWippedCream+
+                        "\nChocolate: "+ addChocolate+
+                        "\nTotal: $"+price+
+                        "\nThank You";
+        return message;
+    }
+
+    public int calculatePrice(boolean addWipppedCream,boolean addChocolate){
+        int price =0;
+
+        price=quantity*5;
+
+        if (addWipppedCream){
+            price+=1;
+        }
+        if (addChocolate){
+            price+=2;
+        }
+        return price;
     }
 }
