fb5cfd3632d07b29fdca427d6778b7020254ecbe
Ahmad <ataufiq665@gmail.com>
2018-02-07 07:26:46 +0700

    add Funnction Plus Button

--- Udacity Course/Basics User Interfaces/CourtCounter/app/src/main/java/ataufiq/com/courtcounter/MainActivity.java
+++ Udacity Course/Basics User Interfaces/CourtCounter/app/src/main/java/ataufiq/com/courtcounter/MainActivity.java
@@ -2,12 +2,60 @@ package ataufiq.com.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView scoreATxt,scoreBTxt;
+    int scoreTeamA,scoreTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreATxt=findViewById(R.id.score_a_txt);
+        scoreBTxt=findViewById(R.id.score_b_txt);
+    }
+
+    public void displayScoreA(int score){
+     scoreATxt.setText(""+score);
+    }
+
+    public void plus3TeamA(View v){
+        scoreTeamA+=3;
+        displayScoreA(scoreTeamA);
+    }
+
+    public void plus2TeamA(View v){
+        scoreTeamA+=2;
+        displayScoreA(scoreTeamA);
+    }
+
+    public void plus1TeamA(View v){
+        scoreTeamA++;
+        displayScoreA(scoreTeamA);
+    }
+
+    //------------------------------------------
+
+    public void displayScoreB(int score){
+        scoreBTxt.setText(""+score);
+    }
+
+    public void plus3TeamB(View v){
+        scoreTeamB+=3;
+        displayScoreB(scoreTeamB);
+    }
+
+    public void plus2TeamB(View v){
+        scoreTeamB+=2;
+        displayScoreB(scoreTeamB);
+    }
+
+    public void plus1TeamB(View v){
+        scoreTeamB++;
+        displayScoreB(scoreTeamB);
     }
 }
