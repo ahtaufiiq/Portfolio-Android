e79db47549cf998fa33a7f89f33c88932ad0da80
Ahmad <ataufiq665@gmail.com>
2018-02-08 13:01:30 +0700

    Adding TextView from Java to XML

--- Udacity Course/2. Basics Multiscreen/MiwokApp/app/src/main/java/ataufiq/com/miwokapp/NumbersActivity.java
+++ Udacity Course/2. Basics Multiscreen/MiwokApp/app/src/main/java/ataufiq/com/miwokapp/NumbersActivity.java
@@ -2,6 +2,10 @@ package ataufiq.com.miwokapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        for (int index=0; index < words.size(); index++){
+            TextView wordview = new TextView(this);
+            wordview.setText(words.get(index));
+            rootView.addView(wordview);
+        }
+
     }
 }
