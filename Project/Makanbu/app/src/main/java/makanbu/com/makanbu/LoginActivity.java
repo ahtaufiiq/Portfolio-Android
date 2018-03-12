2a24cc59f4efa36df47af75bfb8025a26cb66941
Ahmad <ataufiq665@gmail.com>
2018-03-13 00:51:08 +0700

    Add Sign In Feature From Registrasi Akun

--- Project/Makanbu/app/src/main/java/makanbu/com/makanbu/LoginActivity.java
+++ Project/Makanbu/app/src/main/java/makanbu/com/makanbu/LoginActivity.java
@@ -2,8 +2,20 @@ package makanbu.com.makanbu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by SP-SHOCK on 2/21/2018.
@@ -11,14 +23,55 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "EmailPassword";
+    EditText mEmail,mPassword;
+    Button login;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail=findViewById(R.id.user);
+        mPassword=findViewById(R.id.password);
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        login=findViewById(R.id.lgn_btn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn(mEmail.getText().toString(),mPassword.getText().toString());
+            }
+        });
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
+
+    private void signIn(String email, String password) {
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, Home.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        
+                    }
+                });
+        // [END sign_in_with_email]
     }
 }
